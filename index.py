15e3df4fdcf0134e0df6d50e1b1b674a9bb3d7d1
ahaanlimaye <ahaan.limaye@gmail.com>
2021-04-05 13:04:24 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,39 @@
+import re
+import spacy
+from spacy.matcher import Matcher
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe("merge_noun_chunks")
+nlp.add_pipe("merge_entities")
+
+def merge_phone_number(doc):
+    matcher = Matcher(nlp.vocab)
+    pattern = [[{'ORTH': '('}, {'SHAPE': 'ddd'},{'ORTH': ')'},{'ORTH': '-', 'OP': '?'},{'SHAPE': 'ddd'},{'ORTH': '-', 'OP': '?'},{'SHAPE': 'dddd'}],
+               [{'SHAPE': 'ddd'},{'ORTH': '-'},{'SHAPE': 'ddd'},{'ORTH': '-'},{'SHAPE': 'dddd'}]]
+    matcher.add('PHONE_NUMBER', pattern)
+    matches = matcher(doc)
+    for i in range(len(matches)-1, -1, -1):
+        with doc.retokenize() as retokenizer:
+            retokenizer.merge(doc[matches[i][1]:matches[i][2]], attrs={"ENT_TYPE": "PHONE_NUMBER"})
+
+
+def anonymize_text(str):
+    text = (str)
+    doc = nlp(text)
+    merge_phone_number(doc)
+    text = ""
+    ent_types = ["PERSON", ]
+    for token in doc:
+        txt = ""
+        if (token.like_email):
+            txt = "<EMAIL>"
+        elif (len(token.ent_type_) > 0):
+            txt = "<" + token.ent_type_ + ">"
+        elif (token.pos_ == "PROPN"):
+            txt = "<PROPER_NOUN>"
+        else:
+            txt = token.text
+        text += txt + token.whitespace_
+    return text
+
+inp_text = "My name is Ahaan Limaye. I like to go skiing in Vermont. If you guys have any questions please email me at ahaan.limaye@gmail.com or call me at 908-821-6332."
+print(anonymize_text(inp_text))
