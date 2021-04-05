dc8b6e7e3ac1e000f60e9a8898e1a9a5aacb715e
ahaanlimaye <ahaan.limaye@gmail.com>
2021-04-05 16:52:16 -0400

    Made methods to take in file and created anonymized file

--- anonymize.py
+++ anonymize.py
@@ -1,11 +1,37 @@
 from index import anonymize_text
+from pathlib import Path
+import pdfplumber
 
-f = open('C:/Users/ahaan/OneDrive/Desktop/workspace/sample_email1.txt','r+')
-text = f.read()
-print(text)
-text = anonymize_text(text)
-print(text)
-f.seek(0)
-f.write(text)
-f.truncate()
-f.close()
+def anonymize_pdf(filepath):
+    path = Path(filepath)
+    f = pdfplumber.open(path)
+    text = ""
+    for page in f.pages:
+        text += page.extract_text() + "\n"
+    f.close()
+
+    text = anonymize_text(text)
+
+    path = Path(str(path.parent) + "/" + str(path.stem) + "_anonymized.txt") 
+    print(path)  
+    f = open(path, "w+")
+    f.write(text)
+    f.close()
+
+def anonymize_txt(filepath):
+    path = Path(filepath)
+    f = open(path,"r")
+    text = f.read()
+    f.close()
+
+    text = anonymize_text(text)
+
+    path = Path(str(path.parent) + "/" + str(path.stem) + "_anonymized.txt")  
+    f = open(path, "w+")
+    f.seek(0)
+    f.write(text)
+    f.truncate()
+    f.close()
+
+if __name__ == "__main__":
+    anonymize_pdf("C:\\Users\\ahaan\\OneDrive\\Desktop\\workspace\\Resume-AhaanLimaye.pdf")
